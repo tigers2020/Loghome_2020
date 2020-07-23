2eabe3fa5f1bad65038c28438b40627b0350af91
hyper <tigers2020@gmail.com>
2020-07-23 13:41:52 -0400

    2020-07-22 add map

--- static_files/admin/src/js/admin/admin.js
+++
@@ -1,6 +0,0 @@
-import './addons/mdb-vector-map'
-import './addons/moment.min'
-import './addons/fullcalendar.min'
-import './addons/wysiwyg'
-import '../../js/vendor/addons/datatables2'
-import '../../js/vendor/addons/datatables-select2'
